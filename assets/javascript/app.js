ce0d377b6681ece696b61ad4c552be452694a58e
BETH-A <elizabeth78@me.com>
2018-03-16 23:48:11 -0400

    TrainScheduler

---
+++ assets/javascript/app.js
@@ -0,0 +1,11 @@
+var config = {
+    apiKey: "AIzaSyAyQmLi3gIII0DWU9QmKiriuada4IIDeyI",
+    authDomain: "train-scheduler-82f13.firebaseapp.com",
+    databaseURL: "https://train-scheduler-82f13.firebaseio.com",
+    projectId: "train-scheduler-82f13",
+    storageBucket: "",
+    messagingSenderId: "993102084630"
+  };
+  firebase.initializeApp(config);
+
+  
